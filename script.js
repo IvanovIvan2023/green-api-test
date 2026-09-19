f59eaacb6933192d2fc4b43278a1a5142085ee2c
romario <boriso-rom@yandex.ru>
2026-09-19 18:37:16 +0300

    Implement getStateInstance API request

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const getSettingsButton = document.getElementById("getSettingsButton");
+const getStateInstanceButton = document.getElementById("getStateInstanceButton");
 
 getSettingsButton.addEventListener("click", function () {
     const idInstance = document.getElementById("idInstance").value;
@@ -7,6 +8,21 @@ getSettingsButton.addEventListener("click", function () {
     const apiUrl = "https://7201.api.green-api.com";
 
     const url = `${apiUrl}/waInstance${idInstance}/getSettings/${apiTokenInstance}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            const responseField = document.getElementById("response");
+            responseField.value = JSON.stringify(data, null, 2);
+        });
+});
+getStateInstanceButton.addEventListener("click", function () {
+    const idInstance = document.getElementById("idInstance").value;
+    const apiTokenInstance = document.getElementById("apiTokenInstance").value;
+
+    const apiUrl = "https://7201.api.green-api.com";
+
+    const url = `${apiUrl}/waInstance${idInstance}/getStateInstance/${apiTokenInstance}`;
 
     fetch(url)
         .then(response => response.json())
